4365fb9aa2c128c7a32da7ff2d867fb681fcdd4e
kelvin <chengwj@bc.edu>
2020-04-23 21:51:06 -0700

    Set up sound play view

---
+++ ChilLax/SoundViewController.swift
@@ -0,0 +1,91 @@
+//
+//  SoundViewController.swift
+//  ChilLax
+//
+//  Created by Kelvin Cheng on 4/22/20.
+//  Copyright © 2020 Kelvin Cheng. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController {
+    @IBOutlet weak var soundNameLabel: UILabel!
+    @IBOutlet weak var placeDescriptionLabel: UILabel!
+    @IBOutlet weak var sceneryView: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "resMorning")!)
+        backgroundView.backgroundColor = UIColor.clear
+    }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName)
+        {
+            do
+            {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+                Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateAudioProgressView), userInfo: nil, repeats: true)
+                progressView.setProgress(Float(audioPlayer.currentTime / audioPlayer.duration), animated: false)
+            }
+            catch
+            {
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        }
+        else
+        {
+            print("ERROR: Could not read data from sound file.")
+        }
+    }
+    
+    @IBAction func playButtonPressed(_ sender: Any) {
+        if !audioPlayer.isPlaying {
+            playSound(soundName: "Nezuko's_Theme", audioPlayer: &audioPlayer)
+        }
+    }
+    
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+        audioPlayer.pause()
+    }
+    
+    @objc func updateAudioProgressView() {
+       if audioPlayer.isPlaying {
+           progressView.setProgress(Float(audioPlayer.currentTime/audioPlayer.duration), animated: true)
+          }
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        var time: TimeInterval = audioPlayer.currentTime
+        time += 15.0
+          if time > audioPlayer.duration {
+            audioPlayer.stop()
+          } else {
+             audioPlayer.currentTime = time
+          }
+        
+    }
+    
+    @IBAction func forwardButtonPressed(_ sender: Any) {
+        var time: TimeInterval = audioPlayer.currentTime
+        time += 15.0
+          if time > audioPlayer.duration {
+            audioPlayer.stop()
+          } else {
+             audioPlayer.currentTime = time
+          }
+    }
+}
--- ChilLax/ViewController.swift
+++ ChilLax/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var sounds = ["Forest Breeze", "Oceanside Waves", "Rainstorm", "Music"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+    
+    
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = sounds[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
